4df75b14d33f373a02c883f14bb516d388a6d555
thas547 <thas@unc.edu>
2026-04-10 19:17:50 -0400

    done

--- exercises/ex08/linked_list.py
+++ exercises/ex08/linked_list.py
@@ -25,6 +25,34 @@ class Node:
         else:
             return sum(head.next) + head.value  # next nodse sum + my value
 
+    def value_at(head: Node | None, index: int) -> int:
+        if head is None:
+            raise IndexError("Index is out of bounds on the list.")
+        if index == 0:
+            return head.value
+        return value_at(head.next, index - 1)
+
+    def max(head: Node | None) -> int:
+        if head is None:
+            raise ValueError("Canot call max with None")
+        if head.next is None:
+            return head.value
+        rest = max(head.next)
+        if head.value > rest:
+            return head.value
+        else:
+            return rest
+
+    def linkify(items: list[int]) -> Node | None:
+        if len(items) == 0:
+            return None
+        return Node(items[0], linkify(items[1:]))
+
+    def scale(head: Node | None, factor: int) -> Node | None:
+        if head is None:
+            return None
+        return Node(head.value * factor, scale(head.next, factor))
+
 
 node_c: Node = Node(4, None)
 node_b: Node = Node(0, node_c)
